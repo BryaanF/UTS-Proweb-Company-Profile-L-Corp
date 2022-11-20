edd3fa5b5439f4236ed48b1b111fa10ed5f1e12b
FearlessDev <42240127+BryaanF@users.noreply.github.com>
2022-11-20 12:39:39 +0700

    push final ver 1 UTS Proweb

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function formAlert(name) {
+  alert(
+    `Thanks for your message ${name}! We will follow up your message any time soon!`
+  );
+}
+function goToContact() {
+  window.location.href = "contact.html";
+}
